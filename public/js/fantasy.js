068530f95701739f1d1b74851003a8036395f949
Dan Ross <danross826@gmail.com>
2016-08-31 15:02:03 -0500

    I worked on the personal site some more, a lore more animations and have the project image change on an interval

--- public/js/fantasy.js
+++ public/js/fantasy.js
@@ -2,7 +2,6 @@
     'use strict'
 
     $(window).scroll(function() {
-        console.log('in scroll');
         $('.sectionTitle').each(function(){
             var imagePos = $(this).offset().top;
 
@@ -14,13 +13,49 @@
         });
     });
 
-setInterval(function(){
-    $('#projectImage').attr('src','IMG/bodiam_castle.jpg')
-    }, 3000);
+    $(window).scroll(function() {
+        $('.enterLeft').each(function(){
+            var imagePos = $(this).offset().top;
 
-setInterval(function(){
-    $('#projectImage').attr('src','IMG/$_32.jpeg')
-}, 6000);
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+600) {
+                $(this).css("visibility", "visible");
+                $(this).addClass("slideInLeft")
+            }
+        });
+    });
+
+    $(window).scroll(function() {
+        $('.enterRight').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+600) {
+                $(this).css("visibility", "visible");
+                $(this).addClass("slideInRight")
+            }
+        });
+    });
+
+var images = [], x = -1;
+images[0] = "../IMG/avatarscreenshotthree.png";
+images[1] = "../IMG/calculatorOne.png";
+images[2] = "../IMG/weather_map.png";
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("projectImage").src = images[x];
+}
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("projectImage").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
 
 
 // });
