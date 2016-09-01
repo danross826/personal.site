67afbc8be80bda2837c6b419291626da2d67dfe4
Dan Ross <danross826@gmail.com>
2016-09-01 13:05:24 -0500

    I finally have github working again, pushing changes as of Sept 1st to personal site

--- public/js/fantasy.js
+++ public/js/fantasy.js
@@ -37,6 +37,18 @@
         });
     });
 
+    $(window).scroll(function() {
+        $('.enterBottom').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+600) {
+                $(this).css("visibility", "visible");
+                $(this).addClass("slideInUp")
+            }
+        });
+    });
+
 var images = [], x = -1;
 images[0] = "../IMG/avatarscreenshotthree.png";
 images[1] = "../IMG/calculatorOne.png";
