f211ce4b79b173cc81a64584853b4e5e4e33e86b
Dan Ross <danross826@gmail.com>
2016-09-02 16:39:03 -0500

    committing changes as of 09-02-16

--- public/js/fantasy.js
+++ public/js/fantasy.js
@@ -1,6 +1,8 @@
 // $(document).ready( function() {
     'use strict'
 
+
+
     $(window).scroll(function() {
         $('.sectionTitle').each(function(){
             var imagePos = $(this).offset().top;
@@ -48,6 +50,20 @@
             }
         });
     });
+
+$(window).scroll(function() {
+    $('.enterRoll').each(function(){
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+600) {
+            $(this).css("visibility", "visible");
+            $(this).addClass("rollIn")
+        }
+    });
+});
+
+
 
 var images = [], x = -1;
 images[0] = "../IMG/avatarscreenshotthree.png";
